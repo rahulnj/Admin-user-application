921f1368f2c4c869444b28b578c09c17582d7502
Rahul <rahulkvr7a.rnj@gmail.com>
2021-09-20 11:36:02 +0530

    fourth

--- routes/login.js
+++ routes/login.js
@@ -5,6 +5,10 @@ var MongoClient = require('mongodb').MongoClient
 router.get('/', function (req, res, next) {
   res.render('login',);
 });
+router.post('/loggedin', (req, res) => {
+  console.log(req.body);
+  res.send('loggedin')
+})
 
 
 
