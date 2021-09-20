c640a417a924772338a53c831d5c7679222fd58e
Rahul <rahulkvr7a.rnj@gmail.com>
2021-09-20 11:05:11 +0530

    third

--- routes/signup.js
+++ routes/signup.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
+var MongoClient = require('mongodb').MongoClient
+const connection_string = 'mongodb://localhost:27017'
 /* GET signup page. */
 router.get('/', function (req, res, next) {
   res.render('signup')
 });
 router.post('/submit', (req, res) => {
   console.log(req.body);
+  //mongo connection
+  MongoClient.connect(connection_string, (err, client) => {
+    if (err) {
+      console.log('database error');
+      throw error
+
+    } else {
+      console.log('database connected');
+      var db = client.db('ecart')
+      db.collection('newusers').insertOne(req.body)
+    }
+  })
+  res.send('recieved')
 })
+
+
+
 module.exports = router;
